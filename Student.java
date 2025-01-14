89a8fbbaadbbe5b2a08d78e93ddbe946a4bffac0
Prashna <sthaprashna047@gmail.com>
2025-01-14 09:58:45 +0545

    First commit

---
+++ Student.java
@@ -0,0 +1,18 @@
+package w8w8;
+
+public class Student {
+	String name;
+	int age;
+	/**
+	 * Constructor with specified details
+	 * @param name
+	 * @param age
+	 */
+	public Student(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+	
+}
+
